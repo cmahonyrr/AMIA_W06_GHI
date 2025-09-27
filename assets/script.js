7646043e82ecfb8116b9d8633efececad44aace5
Rodrigo Chacon <rodrixchacon@gmail.com>
2025-09-27 15:04:03 -0500

    Subiendo primeros cambios de la navbar

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-
 const btn = document.querySelector('.nav-toggle');
 const links = document.querySelector('#nav-links');
 if(btn){
@@ -7,3 +6,16 @@ if(btn){
     btn.setAttribute('aria-expanded', String(open));
   });
 }
+
+/*  SCRIPT PARA MOSTRAR MENU */
+const mostrarMenu = (toggledId, navId) => {
+  const toggle = document.getElementById(toggledId),
+    nav = document.getElementById(navId)
+
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('show-menu');
+    toggle.classList.toggle('show-icon');
+  });
+}
+
+mostrarMenu('nav-toggle', 'nav-menu');
