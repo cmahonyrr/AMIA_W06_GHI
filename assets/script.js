90be0fbed6aea3c4ca75d154d2ebbc5b94363bca
Rodrigo Chacon <rodrixchacon@gmail.com>
2025-09-28 04:14:03 -0500

    NAVBAR TERMINADO VERSION 1.0

--- assets/script.js
+++ assets/script.js
@@ -17,5 +17,27 @@ const mostrarMenu = (toggledId, navId) => {
     toggle.classList.toggle('show-icon');
   });
 }
-
 mostrarMenu('nav-toggle', 'nav-menu');
+
+/*  SCRIPT PARA EL DROPWDOWN */
+document.querySelectorAll('.dropdown__item .nav__link').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const item = this.parentElement;
+    // Cierra otros dropdowns si quieres solo uno abierto
+    document.querySelectorAll('.dropdown__item').forEach(i => {
+      if(i !== item) i.classList.remove('open');
+    });
+    item.classList.toggle('open');
+  });
+});
+
+// Cierra el dropdown al hacer clic fuera
+document.addEventListener('click', function(e) {
+  document.querySelectorAll('.dropdown__item.open').forEach(item => {
+    // Si el click NO fue dentro del dropdown__item, ciérralo
+    if (!item.contains(e.target)) {
+      item.classList.remove('open');
+    }
+  });
+});
